0719ed696b5c6c2b608cfdb06b196b4bff26d2fa
ygqygq2 <ygqygq2@qq.com>
2022-07-13 23:23:43 +0800

    refactor: 修改基础函数定义位置
    Signed-off-by: ygqygq2 <ygqygq2@qq.com>

--- functions/base.sh
+++ functions/base.sh
@@ -106,3 +106,17 @@ function user_pass_function () {
         esac
     done
 }
+
+function initArch() {
+  ARCH=$(uname -m)
+  case $ARCH in
+    armv5*) ARCH="armv5";;
+    armv6*) ARCH="armv6";;
+    armv7*) ARCH="arm";;
+    aarch64) ARCH="arm64";;
+    x86) ARCH="386";;
+    x86_64) ARCH="amd64";;
+    i686) ARCH="386";;
+    i386) ARCH="386";;
+  esac
+}
--- functions/system.sh
+++ functions/system.sh
@@ -17,20 +17,6 @@ function print_sys_info() {
     df -h
 }
 
-function initArch() {
-  ARCH=$(uname -m)
-  case $ARCH in
-    armv5*) ARCH="armv5";;
-    armv6*) ARCH="armv6";;
-    armv7*) ARCH="arm";;
-    aarch64) ARCH="arm64";;
-    x86) ARCH="386";;
-    x86_64) ARCH="amd64";;
-    i686) ARCH="386";;
-    i386) ARCH="386";;
-  esac
-}
-
 function set_timezone() {
     blue_echo "Setting timezone..."
     rm -f /etc/localtime
